4cb8b1aed3ecb7cbaa3c8bc1a1aeee1fd46b5ba2
ratnakarmishra <ratnakarmishra786@gmail.com>
2019-01-24 21:08:13 +0530

    java examples

---
+++ FindAverage.java
@@ -0,0 +1,10 @@
+class FindAverage
+{
+public static void main(String...k)
+{
+java.util.Scanner sc=new java.util.Scanner(System.in);
+System.out.println("enter sub marks");
+float sub1=sc.nextFloat();
+float sub2=sc.nextFloat();
+System.out.println("avg="+(sub1+sub2/2));
+}}
